000ddcdb9ccde841d2add849b7e80b3d82b325f1
Zubair Zafar <zubairzafar480@gmail.com>
2023-09-08 10:48:37 +0500

    added followers tree

---
+++ src/SmartParser/parser/FollowersTree.py
@@ -0,0 +1,11 @@
+
+class Node:
+    def __init__(self,children,parent):
+        self.children = children
+        self.parent = parent
+
+    
+class FollowersTree:
+
+    def __init__(self): pass        
+    
